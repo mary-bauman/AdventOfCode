283c64541150e7ae4e314a30b4e35a69b69e1f86
marybauman <marybauman503@gmail.com>
2024-01-05 17:09:45 -0500

    bouta get ai up in here

--- 2023/20/20-2.py
+++ 2023/20/20-2.py
@@ -26,61 +26,35 @@ with open("in.txt") as f:
             broadcaster = two.split(", ")
             modType["broadcaster"] = prefix
             lastPulse["broadcaster"] = "low"
-# print(f"d = {d}")
-# print(f"broadcaster = {broadcaster}")
-
 
 
 def broadCast(onOff):
-    lowPulses = 0
-    highPulses = 0
     q = deque()
     q.append(("broadcaster", "low", "button"))
-    lowPulses += 1
     for b in broadcaster:
-        #b keyOfDest were gonna get, pulse (low = default), prev command
         q.append((b, "low", "broadcaster"))
-        lowPulses += 1
-        # print(b)
-   
-
-    def flipFlop(s):
-        print("flipFlop")
-
-    def conjunction(s):
-        print("conjunction")
-
-
-    # print(f"queue = {q}")
 
     while(len(q)>0):
         endFound = False
         item = q.popleft()
-        #print(f"item = {item}")
         keyOfDest = item[0]
         pulse = item[1]
-        prevCommand = item[2]
         if keyOfDest == END_MODULE:
-            print(f"{prevCommand} -{pulse}-> {keyOfDest}")
+            #print(f"{item[2]} -{pulse}-> {keyOfDest}")
             if(pulse=="low"):
                 endFound = True
             continue
         prefix = modType[keyOfDest]
-        # print(f"{prevCommand} -{pulse}-> {keyOfDest}")
-        # if prefix == 'b':
-        #     print("why does keyOfDest equal b")
         if prefix == '%':
             if pulse == "low":
                 if onOff[keyOfDest] == "off":
                     for dest in d[keyOfDest]:
                         q.append((dest, "high", keyOfDest))
-                        highPulses += 1
                     onOff[keyOfDest] = "on"
                     lastPulse[keyOfDest] = "high"
                 else:
                     for dest in d[keyOfDest]:
                         q.append((dest, "low", keyOfDest))
-                        lowPulses += 1
                     onOff[keyOfDest] = "off"
                     lastPulse[keyOfDest] = "low"
         elif prefix == '&':
@@ -93,16 +67,13 @@ def broadCast(onOff):
             if allHigh:
                 for dest in d[keyOfDest]:
                     q.append((dest, "low", keyOfDest))
-                    lowPulses += 1
                 lastPulse[keyOfDest] = "low"
             else:
                 for dest in d[keyOfDest]:
                     q.append((dest, "high", keyOfDest))
-                    highPulses += 1
                 lastPulse[keyOfDest] = "high"
     return (endFound)
 
-amountOfBroadCasts = 1000
 onOffStart = defaultdict(lambda: "off")
 count = 0
 endFound = False
