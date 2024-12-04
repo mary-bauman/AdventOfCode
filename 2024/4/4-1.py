b2924e42525186784d85c3b772e21e329ef5e653
Mary <marybauman503@gmail.com>
2024-12-04 12:10:13 -0500

    4

--- 2024/4/4-1.py
+++ 2024/4/4-1.py
@@ -1,6 +1,5 @@
 numLines = 140
-lines = []
-for _ in range(numLines): lines.append(input())
+lines = [input() for _ in range(numLines)]
 total = 0
 xmas = ["XMAS", "SAMX"]
 rowLen = len(lines[0])
@@ -14,8 +13,6 @@ for col in range(colLen):
     for i in range(colLen-3):
         if "".join([lines[j][col] for j in range(i, i+4)]) in xmas: total += 1
 #diagonal left to right down(backwards and forwards)
-#lines[0][0],lines[1][1],lines[2][2],lines[3][3]
-#lines[0][1],lines[1][2],lines[2][3],lines[3][4]
 for row in range(colLen-3):
     for col in range(rowLen-3):
         if "".join([lines[row+j][col+j] for j in range(4)]) in xmas: total += 1
