322feb5d3f01098130f3a3460229e3535bcae8f6
Mary <marybauman503@gmail.com>
2024-12-09 12:48:07 -0500

    9-1

--- 2024/9/9-1.py
+++ 2024/9/9-1.py
@@ -7,13 +7,10 @@ for i in range(len(line)):
 
 s = []
 for i in range(len(space)):
-    # s += str(i)*int(files[i])
     for _ in range(int(files[i])): s.append(str(i))
-    # s+="."*int(space[i])
     for _ in range(int(space[i])): s.append(".")
 if len(files)>len(space):
     for _ in range(int(files[-1])): s.append(str(len(files)-1))
-# print(s)
 finalS = []
 i = 0
 n = len(s)
@@ -21,12 +18,11 @@ while i < n:
     if s[i] != ".":
         finalS.append(s[i])
     else:
-        while s[n-1] == "." and n>0:
-            n-=1
+        while s[n-1] == "." and n>0: n-=1
         finalS.append(s[n-1])
         n-=1
     i+=1
-print(finalS)
+    
 total = 0
 for i in range(len(finalS)):
     total += int(finalS[i]) * (i)
