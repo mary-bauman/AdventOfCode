05d48ea6201607c012d06d4683acfa28ae99a309
Mary <marybauman503@gmail.com>
2025-01-04 16:50:41 -0500

    day 12 part 1

--- 2024/12/12-1.py
+++ 2024/12/12-1.py
@@ -1,46 +1,48 @@
-numLines = 10
-arr = [input() for _ in range(numLines)]
-regions = dict()
-#regions[char] = [[(x,y) for xy in region]]
-numRows = len(arr)
-numCols = len(arr[0])
-for r in range(numRows):
-    for c in range(numCols):
-        char = arr[r][c]
-        if char not in regions:
-            regions[char] = [[(r,c)]]
-        else:
-            #go through and make sure regions is combined
-            #then look for region with neighbor for this
-            #otherwise add this as a new region
+rows = 10
+rows = 140
+arr = [list(input()) for _ in range(rows)]
+cols = len(arr[0])
+for a in arr: print(a)
+
+def area(region):
+    return len(region)
+
+def perimeter(region):
+    sides = 0
+    for r,c in region:
+        for dr,dc in [(0,1),(1,0),(-1,0),(0,-1)]:
+            if (r+dr,c+dc) not in region:
+                sides += 1
+    return sides
+
+ans = 0
+done = set()
+for r in range(rows):
+    for c in range(cols):
+        if (r,c) not in done:
+            region = []
+            stack = [(r,c)]
+            char = arr[r][c]
+            while stack:
+                x,y = stack.pop()
+                # print("x = ", x, "y = ", y)
+                if arr[x][y]==char and (x,y) not in done:
+                    done.add((x,y))
+                    region.append((x,y))
+                    for dx,dy in [(0,1),(1,0),(-1,0),(0,-1)]:
+                        if x+dx>=0 and x+dx<rows and y+dy>=0 and y+dy<cols:
+                            stack.append((x+dx,y+dy))
 
 
-# print(regions)
+            # print(f"region for {char} = ")
+            # for reg in region: print(reg)
+            total = area(region)
+            p = perimeter(region)
+            print("area = ", total, "perimeter = ", p)
+            total *= p
+            ans += total
+            done.add((r,c))
+        # print("c = ", c)
 
-
-def shape_perimeter(points):
-    perimeter = 0
-    directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-    for point in points:
-        x, y = int(point[0]), int(point[1])
-        for dx, dy in directions:
-            neighbor = (x + dx, y + dy)
-            if neighbor not in points:
-                perimeter += 1
-    return perimeter
-
-
-total = 0
-for char in regions:
-    a = regions[char]
-    for region in a:
-        print(region)
-        area = len(region)
-        perimeter = shape_perimeter(region)
-        print(f"Region {char}: Area = {area}, Perimeter = {perimeter}")
-        total += area * perimeter
-
-
-print(total)
-
+print("ans = ", ans)
 
