1016a0288aa550c804cdcd58ecdf7d2cae20576d
Mary <marybauman503@gmail.com>
2025-11-07 20:00:58 -0500

    1 done

--- 2022/1/main.py
+++ 2022/1/main.py
@@ -1,7 +1,17 @@
-with open("in-test.txt") as f:
+# with open("in-test.txt") as f:
+with open("in.txt") as f:
     line = f.readline().strip()
     a = []
-    while line:
-      print(line)
-        
+    bestCal = 0
+    cur = 0
+    # lines = 14
+    lines = 2237
+    for l in range(lines):
+      line = f.readline().strip()
+      if line == "":
+        bestCal = max(bestCal, cur)
+        cur = 0
+      else:
+        cur += int(line)
+    print(bestCal)   
     
