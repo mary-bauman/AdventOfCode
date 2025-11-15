df439c83724472ee1c95990d0b4f6716ff593057
Mary <marybauman503@gmail.com>
2025-11-15 16:26:20 -0500

    3-2 passing

--- 2022/3/3-2.py
+++ 2022/3/3-2.py
@@ -1,6 +1,19 @@
-with open("in-test.txt") as f:
-  lines = 0
-  for _ in range(lines):
-    line = f.readline().strip()
-        
+# with open("in-test.txt") as f:
+#   lines = 6
+with open("in.txt") as f:
+  lines = 300
+  both = 0
+  for _ in range(lines//3):
+    first = f.readline().strip()
+    second = f.readline().strip()
+    third = f.readline().strip()
+    done = []
+    for c in first:
+      if c in second and c in third and (c not in done):
+        both += ord(c) - (96 if c.islower() else 38)
+        done.append(c)
     
+
+  print(both)
+    
+        
