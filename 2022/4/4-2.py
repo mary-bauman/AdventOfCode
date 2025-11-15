049e4c2f2c1a65a8a5fac567988b78443b45c25e
Mary <marybauman503@gmail.com>
2025-11-15 16:52:56 -0500

    4-2 passing

--- 2022/4/4-2.py
+++ 2022/4/4-2.py
@@ -1,6 +1,17 @@
-with open("in-test.txt") as f:
-  lines = 0
+# with open("in-test.txt") as f:
+#   lines = 6
+with open("in.txt") as f:
+  lines = 1000
+  ans = 0
   for _ in range(lines):
-    line = f.readline().strip()
+    one, two = f.readline().strip().split(",")
+    s1, b1 = map(int, one.split("-"))
+    s2, b2 = map(int, two.split("-"))
+    if s1 <= s2 and b1 >= b2: ans +=1
+    elif s2 <= s1 and b2 >= b1: ans +=1
+    elif s1<=s2 and b1>=s2: ans +=1
+    elif s2<=s1 and b2>=s1: ans +=1
+
+  print(ans)
         
     
