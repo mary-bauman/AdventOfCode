2bc9c34612ebc9c4f862fbcab560a063bf9055f7
Mary <marybauman503@gmail.com>
2025-11-15 16:18:14 -0500

    3.1 pass

--- 2022/3/3-1.py
+++ 2022/3/3-1.py
@@ -1,19 +1,19 @@
 from collections import Counter
 
-with open("in-test.txt") as f:
-  lines = 6
+# with open("in-test.txt") as f:
+  # lines = 6
+with open("in.txt") as f:
+  lines = 300
   both = 0
   for _ in range(lines):
     line = f.readline().strip()
-    first, second = line[:len(line)//2].split(), line[len(line)//2:].split()
+    first, second = line[:len(line)//2], line[len(line)//2:]
     done = []
     for c in first:
       if c not in done and c in second:
         both += ord(c) - (96 if c.islower() else 38)
         done.append(c)
     
-    print(both)
-
 
   print(both)
     
