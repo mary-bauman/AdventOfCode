9a2fdd951185c4ef61938b4a1bd86129b2910e00
Mary <marybauman503@gmail.com>
2025-12-06 15:52:08 -0500

    6-1 accepted

--- 2025/6/6-1.py
+++ 2025/6/6-1.py
@@ -1,5 +1,24 @@
-# with open("in-test.txt") as f:
 with open("in.txt") as f:
-    for _ in range(freshLines):
-        line = f.readline().strip().split("-")
-        
+    a = []
+    numRows = 4
+    for _ in range(numRows):
+        a.append(f.readline().strip().split())
+    ops = f.readline().strip().split()
+    numCols = len(a[0])
+
+    total = 0
+    for col in range(numCols):
+        ans = 0
+        op = ops[col]
+        match op:
+            case "+":
+                for row in range(numRows):
+                    ans += int(a[row][col])
+            case "*":
+                ans = 1
+                for row in range(numRows):
+                    ans *= int(a[row][col])
+
+        total += ans
+    
+    print(total)
