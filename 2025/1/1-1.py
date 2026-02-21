00662f730e46d8d6eb4ffd76d2a80bab483c1e8b
Mary <marybauman503@gmail.com>
2026-02-21 14:59:35 -0500

    1-2 accepted

--- 2025/1/1-1.py
+++ 2025/1/1-1.py
@@ -1,4 +1,12 @@
-# with open("in-test.txt") as f:
+numLines = 4493
 with open("in.txt") as f:
-    line = f.readline().strip()
-    
+    dial = 50
+    pw = 0
+    for _ in range (numLines) :
+        line = f.readline().strip()
+        i = int(line [1:])
+        if line [0]=="L": i = (100-i)
+        dial += i 
+        dial %= 100
+        if dial==0: pw+=1
+    print (pw)
